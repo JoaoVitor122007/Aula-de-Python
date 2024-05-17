6679dd66c41462aae391b8cc8c21c0969b6887a0
JoaoVitor122007 <163335492+JoaoVitor122007@users.noreply.github.com>
2024-05-17 15:57:55 -0300

    Create att03.py

---
+++ att03.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+num3 = int(input("Digite o terceiro numero: "))
+
+media = (num1 + num2 + num3) / 3
+
+print("A media dos numeros é: ", media)
